158eae5bbb7440c460ef1da8b191c10cbfaa2caf
minotte <n.minotte@gmail.com>
2025-02-13 14:26:06 -0500

    add docstring and modify parsing to don't have special character

--- Starting/ex06/filterstring.py
+++ Starting/ex06/filterstring.py
@@ -5,6 +5,20 @@ from ft_filter import ft_filter
 
 
 def main():
+    """
+    it's main fonction of the programme and call ft_filter to
+    The program should return every word from S 
+    that have a length greater than N.
+    args:
+        - 1st : string
+        - 2nd : int
+    Error:
+        - if different than two arguments.
+        - if the argument have punctuation or invisible character
+        - if it's not a int
+    Print:
+        every word from S that have a length greater than N
+    """
     try:
         if len(sys.argv) != 3:
             raise AssertionError("the arguments are bad")
@@ -13,9 +27,12 @@ def main():
             N = int(N)
         except ValueError:
             raise AssertionError("the arguments are bad")
+
         if not isinstance(S, str):
             raise AssertionError("the arguments are bad")
         words = S.split()
+        if not all(word.isalpha() for word in words):
+            raise AssertionError("the arguments are bad")
         filterstring = ft_filter(lambda word: len(word) > N, words)
         print(list(filterstring))
 
