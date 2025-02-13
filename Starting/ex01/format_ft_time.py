f4e25490b00f30f3958df8decafbb1d5aefae2c0
minotte <n.minotte@gmail.com>
2025-02-13 06:20:56 -0500

    change print to show the coma

--- Starting/ex01/format_ft_time.py
+++ Starting/ex01/format_ft_time.py
@@ -6,5 +6,6 @@ from datetime import date, timedelta
 today = date.today()
 now = time.time()
 
-print(f"Seconds since January 1, 1970: {now[1]},{now[2 : 6]}, or {format(now,'.2e')} in scientific notation")
+print(f"Seconds since January 1, 1970: {now:,.4f}, \
+or {format(now,'.2e')} in scientific notation")
 print(today.strftime("%d %B %Y"))
